f22a4e97e4adac5f2ca52d94d4e9c4bc400deb30
Richard5P <richard.deutsch@gmail.com>
2023-07-29 15:44:32 +0000

    feat: Add display functions for home and guide panel; Add hidePanels function; Update onload to call displayGuide to test functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 //Variables - initialise targeted elements
 //  Navigation and Info Bar controls
-const homeInfoButton = document.getElementById("btn-home");
+const homeInfoBarButton = document.getElementById("btn-home");
+
+// Panels
+const panelArea = document.getElementById("panel-area");
+const welcomePanel = document.getElementById("welcome-panel");
+const recUserPanel = document.getElementById("rec-user-panel");
+const quizPanel = document.getElementById("quiz-panel");
+const resultsPanel = document.getElementById("results-panel")
+const guidePanel = document.getElementById("guide-panel");
+const scoresPanel = document.getElementById("scores-panel");
 
 let currentPanel = "guide-panel";
 
 window.onload = function () {
-    displayHomePanel();
+    displayGuidePanel();
 };
 
 // Add event listeners
@@ -13,3 +22,31 @@ window.onload = function () {
 document.addEventListener("DOMContentLoaded", function () {
     // controls Area
     homeInfoBarButton.addEventListener("click", displayHomePanel);
+})
+
+// Define functions
+// hide and display panel functions
+
+function hidePanels() {
+    // hide timers as well
+//        gameTimerDisplay.classList.add("hidden");
+//        questionTimerDisplay.classList.add("hidden");    
+    
+        let panels = document.getElementsByClassName("panel");
+        for (let i = 0; i < panels.length; i++)
+            panels[i].classList.add("hidden");
+    }
+    
+function displayHomePanel() {
+        console.log("Home Panel");
+        hidePanels();
+        welcomePanel.classList.remove("hidden");
+        currentPanel = "welcome-panel";
+    }
+
+function displayGuidePanel() {
+    console.log("Guide Panel");
+    hidePanels();
+    guidePanel.classList.remove("hidden");
+    currentPanel = "guide-panel";    
+}
