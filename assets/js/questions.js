49ce6e5d58f3fa43fc3249aafc8f30def3f43ca7
Richard5P <richard.deutsch@gmail.com>
2023-07-31 08:58:09 +0000

    boilerplate: Add questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+// Question/Answer Object Data
+
+const quizQuestions = [{
+        question: "\'There are decades where nothing happens; and there are weeks where decades happen.\' Is a quote from?",
+        a: "Karl Marx",
+        b: "Leon Trosky",
+        c: "Rosa Luxemburg",
+        d: "Vladimir Lenin",
+        answer: "D",
+        linkto: {
+            docName: "War and Revolution",
+            url: "https://www.marxists.org/archive/lenin/works/1917/may/14.htm",
+        }
+    },
+    {
+        question: "\'A specter is haunting Europe—the specter of Communism.\' was first published in?",
+        a: "Das Kapital",
+        b: "Neue Rheinische Zeitung",
+        c: "The Communist Manifesto",
+        d: "Founding Documents of the Communist League",
+        answer: "c",
+        linkto: {
+            docName: "The Communist Manifesto",
+            url: "https://www.marxists.org/archive/marx/works/1848/communist-manifesto/",
+        },
+    }
+]
