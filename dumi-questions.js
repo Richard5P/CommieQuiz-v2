1fe95a8d3798890e1ccc3cfd6117d8c2c9155c5e
Richard5P <richard.deutsch@gmail.com>
2023-08-09 11:08:25 +0100

    feat:Add rec-user, select-difficult, start-quiz html panels; Update questions to dumi-questions;Add dumi-questions.js

---
+++ dumi-questions.js
@@ -0,0 +1,82 @@
+// create array of question objects
+const quizQuestions = [{
+    q: "Question-01", 
+    a: "Answer-01a",
+    b: "Answer-01b",
+  }, 
+  {
+    q: "Question-02", 
+    a: "Answer-02a",
+    b: "Answer-02b",
+  }, 
+  {
+    q: "Question-03", 
+    a: "Answer-03a",
+    b: "Answer-03b",
+  },
+  {
+    q: "Question-04", 
+    a: "Answer-04a",
+    b: "Answer-04b",
+  }, 
+  {
+    q: "Question-05", 
+    a: "Answer-05a",
+    b: "Answer-05b",
+  }, 
+  {
+    q: "Question-06", 
+    a: "Answer-06a",
+    b: "Answer-06b",
+  },
+  {
+    q: "Question-07", 
+     a: "Answer-07a",
+    b: "Answer-07b",
+  }, 
+  {
+    q: "Question-08", 
+    a: "Answer-08a",
+    b: "Answer-08b",
+  }, 
+  {
+    q: "Question-09", 
+    a: "Answer-09a",
+    b: "Answer-09b",
+  },
+  {
+    q: "Question-10", 
+     a: "Answer-10a",
+    b: "Answer-10b",
+  },
+  {
+    q: "Question-11", 
+    a: "Answer-11a",
+    b: "Answer-11b",
+  }, 
+  {
+    q: "Question-12", 
+    a: "Answer-12a",
+    b: "Answer-12b",
+  }, 
+  {
+    q: "Question-13", 
+    a: "Answer-13a",
+    b: "Answer-13b",
+  },
+  {
+    q: "Question-14", 
+    a: "Answer-14a",
+    b: "Answer-14b",
+  }, 
+  {
+    q: "Question-15", 
+      a: "Answer-15a",
+    b: "Answer-15b",
+  }, 
+  {
+    q: "Question-16", 
+    a: "Answer-16a",
+    b: "Answer-16b",
+  }
+  ]  
